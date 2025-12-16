e8bac5d6835878b8fadd703e5e55fbb8f05f0be4
artockthedino <chadedwardworley@gmail.com>
2025-12-16 09:56:03 -0500

    Proportion problem

---
+++ ml2026/proportions/exams2forms_testing.R
@@ -0,0 +1,14 @@
+library("exams2forms")
+n = 8
+
+### Entrance ticket
+pm = list("proportion_graph_pt2.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_1216",
+             dir = ".",
+             name="examples_1216")
+
+
+
