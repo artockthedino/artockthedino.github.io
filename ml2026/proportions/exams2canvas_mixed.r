cf83791e001ceba00e5f348528f7da179a23ea17
artockthedino <chadedwardworley@gmail.com>
2025-12-17 15:11:11 -0500

    Wed Dec 17

--- ml2026/proportions/exams2canvas_mixed.r
+++ ml2026/proportions/exams2canvas_mixed.r
@@ -1,31 +1,31 @@
 library("exams")
-n = 20
+n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-pm = list("et1_quad_AOS_from_roots_B.Rmd",
-          "et2_quad_w_from_roots.Rmd")
+pm = list("proportion_graph_pt2.Rmd")
 
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=rep(5,length(pm)),
+             maxattempts = 10,
+             name="at1216proportion",
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
+
+# # Classwork 1
+pm = list("proportion_graph_pt2.Rmd",
+          "proportion_graph_pt2.Rmd",
+          "proportion_graph_pt2.Rmd")
 exams2canvas(pm,
              n=n,
              dir=outdir,
              points=rep(2,length(pm)),
              maxattempts = 6,
-             name="at1204entrance",
+             name="at1216prop_MASTERY",
              template = "canvas_qti12.xml",
              cloze = list(enumerate = FALSE))
-
-# # Classwork 1
-# pm = list("01_solve_rect_quadratic.Rmd",
-#           "02_quad_formula_roots.Rmd")
-# exams2canvas(pm,
-#              n=n,
-#              dir=outdir,
-#              points=rep(4,length(pm)),
-#              maxattempts = 6,
-#              name="at1203cA_quad_formula",
-#              template = "canvas_qti12.xml",
-#              cloze = list(enumerate = FALSE))
 
  
 # # # Classwork 2
