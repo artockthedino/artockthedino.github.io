32b856a28cd0c89f0df5f172801112c3ed824fd9
artockthedino <chadedwardworley@gmail.com>
2026-01-03 18:15:02 -0500

    2026_Jan_03

--- algtwo2026/u07/0105/exams2canvas_mixed.r
+++ algtwo2026/u07/0105/exams2canvas_mixed.r
@@ -3,31 +3,30 @@ n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-pm = list("01_i_to_power_intro.Rmd",
-          "02_simplify_sqrt_neg_square.Rmd",
-          "03_simplify_sqrt_neg_nonsqr.Rmd",
-          "04_quadratic_formula_complex.Rmd")
+pm = list("et_qf_radical_solns.Rmd")
 exams2canvas(pm,
              n=n,
              dir=outdir,
              points=rep(1,length(pm)),
              maxattempts = 5,
-             name="a0105c_imaginary_unit",
+             name="a0105et_qf_radical_solns",
              template = "canvas_qti12.xml",
              cloze = list(enumerate = FALSE))
 
 
-# pm = list("maximize_profit.Rmd",
-#           "maximize_profit.Rmd")
+# pm = list("01_i_to_power_intro.Rmd",
+#           "02_simplify_sqrt_neg_square.Rmd",
+#           "03_simplify_sqrt_neg_nonsqr.Rmd",
+#           "04_quadratic_formula_complex.Rmd")
 # exams2canvas(pm,
 #              n=n,
 #              dir=outdir,
-#              points=rep(5,length(pm)),
-#              maxattempts = 8,
-#              name="a1222c1_maximize_profit",
+#              points=rep(1,length(pm)),
+#              maxattempts = 5,
+#              name="a0105c_imaginary_unit",
 #              template = "canvas_qti12.xml",
 #              cloze = list(enumerate = FALSE))
-# 
+
 # pm = list("standard_from_graph.Rmd",
 #           "standard_from_graph.Rmd")
 # exams2canvas(pm,
--- algtwo2026/u07/0105/exams2forms_testing.R
+++ algtwo2026/u07/0105/exams2forms_testing.R
@@ -2,7 +2,8 @@ library("exams2forms")
 n = 10
 
 ### Entrance ticket
-pm = list("01_i_to_power_intro.Rmd",
+pm = list("et_qf_radical_solns.Rmd",
+          "01_i_to_power_intro.Rmd",
           "02_simplify_sqrt_neg_square.Rmd",
           "03_simplify_sqrt_neg_nonsqr.Rmd",
           "04_quadratic_formula_complex.Rmd")
