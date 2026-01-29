8c1701524b2ed21d5bfccc9d600a84f2b1b0f147
artockthedino <chadedwardworley@gmail.com>
2026-01-29 13:58:22 -0500

    fixed typo

--- pbl/daily/d03/exams2forms_testing.R
+++ pbl/daily/d03/exams2forms_testing.R
@@ -2,11 +2,11 @@ library("exams2forms")
 n = 10
 
 ### Entrance ticket
-pm = list("01_tra_rot_gli_mir.Rmd")
+pm = list("x_recognize_Frieze_group.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_day01",
+             title = "examples_day03",
              dir = ".",
-             name="examples_day01")
+             name="examples_day03")
 
