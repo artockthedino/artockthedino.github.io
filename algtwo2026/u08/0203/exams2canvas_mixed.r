8de7f9587eee660be098332d7d3c31793084c0db
artockthedino <chadedwardworley@gmail.com>
2026-02-02 21:49:03 -0500

    fixed some stuff

--- algtwo2026/u08/0203/exams2canvas_mixed.r
+++ algtwo2026/u08/0203/exams2canvas_mixed.r
@@ -3,15 +3,15 @@ n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-pm = list("w_2val_1val_0val.Rmd")
-exams2canvas(pm,
-             n=n,
-             dir=outdir,
-             points=rep(4,length(pm)),
-             maxattempts = 6,
-             name="a0203_et",
-             template = "canvas_qti12.xml",
-             cloze = list(enumerate = FALSE))
+# pm = list("w_2val_1val_0val.Rmd")
+# exams2canvas(pm,
+#              n=n,
+#              dir=outdir,
+#              points=rep(4,length(pm)),
+#              maxattempts = 6,
+#              name="a0203_et",
+#              template = "canvas_qti12.xml",
+#              cloze = list(enumerate = FALSE))
 
 pm = list("x_find_extraneous_graph.Rmd",
           "x_find_valext_graph.Rmd",
@@ -26,17 +26,19 @@ exams2canvas(pm,
              cloze = list(enumerate = FALSE))
 
 
-# pm = list("standard_from_graph.Rmd",
-#           "standard_from_graph.Rmd")
-# exams2canvas(pm,
-#              n=n,
-#              dir=outdir,
-#              points=rep(4,length(pm)),
-#              maxattempts = 8,
-#              name="a1222c2_standard_from_graph",
-#              template = "canvas_qti12.xml",
-#              cloze = list(enumerate = FALSE))
-# 
+pm = list("x_find_valext1_graph.Rmd",
+          "x_find_valext0_graph.Rmd",
+          "x_find_valext2_graph.Rmd",
+          "x_find_valext012_graph.Rmd")
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=rep(1,length(pm)),
+             maxattempts = 8,
+             name="a0203_MASTERY_graph_extraneous",
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
+
 # 
 # pm = list("calculate_discriminant.Rmd",
 #           "discriminant_num_roots.Rmd")
