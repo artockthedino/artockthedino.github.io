a75b636598a3fd49e29a72eda91a4d0f37e3926c
artockthedino <chadedwardworley@gmail.com>
2026-03-05 20:14:42 -0500

    polynomial multiplication

---
+++ algtwo2026/u09/0306/exams2forms_testing.R
@@ -0,0 +1,13 @@
+library("exams2forms")
+n = 6
+
+### Entrance ticket
+pm = list("x_multiply_linear_cubic.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_0306",
+             dir = ".",
+             name="examples_0306")
+
+
