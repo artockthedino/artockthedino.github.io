2fd89a68c2ca12b69b2b3742a9c1de25cc59311a
artockthedino <chadedwardworley@gmail.com>
2026-03-17 13:38:51 -0400

    polynomial division

--- pbl/Lsys_turtle/exams2forms_testing.R
+++ pbl/Lsys_turtle/exams2forms_testing.R
@@ -1,13 +1,12 @@
 library("exams2forms")
 n = 10
 
-### Entrance ticket
-pm = list("which_spirolateral.Rmd","gcfs.Rmd")
+pm = list("turtle_commands_A.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0309",
+             title = "examples_0317",
              dir = ".",
-             name="examples_0309")
+             name="examples_0317")
 
 
