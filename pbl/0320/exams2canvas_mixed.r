294232f8b3a22fb1352457e535e0498b34599d4e
artockthedino <chadedwardworley@gmail.com>
2026-03-20 13:54:29 -0400

    not much

--- pbl/0320/exams2canvas_mixed.r
+++ pbl/0320/exams2canvas_mixed.r
@@ -1,26 +1,32 @@
 library("exams")
-n = 30
+n = 20
 outdir = "outcanvas"
 
 ### Entrance ticket
-pm = list("x_venn_2_dots.Rmd","x_venn_2_dots.Rmd")
+pm = list("x_which_lsys_fractal.Rmd","x_which_lsys_fractal.Rmd")
 exams2canvas(pm,
              n=n,
              dir=outdir,
-             points=c(2,2),
-             maxattempts = 10,
-             name="m0320_ET",
+             points=c(5,5),
+             maxattempts = 5,
+             name="pbl_0320_ET",
              template = "canvas_qti12.xml",
              cloze = list(enumerate = FALSE))
 
-# pm = list("x_multiply_linear_quadratic.Rmd","x_multiply_linear_quadratic.Rmd")
+# pm = c("math_facts_add.Rmd",
+#        "math_facts_sub.Rmd",
+#        "math_facts_mul.Rmd",
+#        "math_facts_div.Rmd")
+# 
+# pm = c(pm,sample(pm),sample(pm))
+# 
 # exams2canvas(pm,
 #              n=n,
 #              dir=outdir,
-#              points=rep(1,length(pm)),
+#              points=rep(2,length(pm)),
 #              maxattempts = 6,
-#              name="a0305_mult_linear_quadratic",
-#              converter = "pandoc-mathjax",
+#              name="pbl_0302_math_facts",
+#              converter = "tex2image",
 #              template = "canvas_qti12.xml",
 #              cloze = list(enumerate = FALSE))
 
---
+++ pbl/0320/exams2forms_testing.R
@@ -0,0 +1,13 @@
+library("exams2forms")
+n = 3
+
+### Entrance ticket
+pm = list("x_which_lsys_fractal.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_0320",
+             dir = ".",
+             name="examples_0320")
+
+
