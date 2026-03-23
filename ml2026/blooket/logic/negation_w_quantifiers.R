6a74ce39cd3fc33a75915c9fcfe972fa5bce1d6b
artockthedino <chadedwardworley@gmail.com>
2026-03-23 12:12:52 -0400

    negation with quantifiers

--- ml2026/blooket/logic/negation_w_quantifiers.R
+++ ml2026/blooket/logic/negation_w_quantifiers.R
@@ -12,21 +12,23 @@ phrases = c("All XXX are YYY.","No XXX are YYY.","Some XXX are YYY.","Some XXX a
 
 explains = c("Set $A$ is a subset of set $B$.","Set $A$ and set $B$ are disjoint.","The intersection of set $A$ and set $B$ is not empty.","Set $A$ is not a subset of set $B$.")
 
-nq = 200
+nq = 800
 for(i in 1:nq){
-    # n = sample(nouns,1)
-    # a = sample(adjectives,1)
-    na = sample(LETTERS,2)
-    n = na[1]
-    a = na[2]
-    phrases2 = gsub("$A$",n,explains,fixed=T)
-    phrases2 = gsub("$B$",a,phrases2,fixed=T)
+    n = sample(nouns,1)
+    a = sample(adjectives,1)
+    phrases2 = gsub("XXX",n,phrases,fixed=T)
+    phrases2 = gsub("YYY",a,phrases2,fixed=T)
+    # na = sample(LETTERS,2)
+    # n = na[1]
+    # a = na[2]
+    # phrases2 = gsub("$A$",n,explains,fixed=T)
+    # phrases2 = gsub("$B$",a,phrases2,fixed=T)
     i1 = sample(1:4,1)
     p1 = phrases2[i1]
     p2 = phrases2[5-i1]
     ach = sample(phrases2)
     wo = which(ach==p2)
-    qtext = paste0("Consider the logical statement (p) defined below.\n  p: ",p1,"\nWhich option represents ~p, the negation of p?")
+    qtext = paste0("Consider the logical statement (p) defined below.\n\n p: ",p1,"\n\nWhich option represents ~p, the negation of p?")
     s=paste0(s,i,',"',qtext,'",',
              paste0(ach,collapse=','),',30,',wo,collapse=""
     )
@@ -35,6 +37,6 @@ for(i in 1:nq){
     }
 }
 
-fileConn<-file("negate_w_quantifiers_SETS.csv")
+fileConn<-file("negate_w_quantifiers.csv")
 writeLines(s, fileConn)
 close(fileConn)
