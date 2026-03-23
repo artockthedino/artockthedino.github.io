156347ca685af4d4dd41e97863432a601b49e097
artockthedino <chadedwardworley@gmail.com>
2026-03-23 13:21:15 -0400

    negation with quantifiers

--- pbl/0323/exams2forms_testing.R
+++ pbl/0323/exams2forms_testing.R
@@ -1,13 +1,13 @@
 library("exams2forms")
-n = 3
+n = 4
 
 ### Entrance ticket
 pm = list("x_which_lsys_fractal.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0320",
+             title = "examples_0323",
              dir = ".",
-             name="examples_0320")
+             name="examples_0323")
 
 
