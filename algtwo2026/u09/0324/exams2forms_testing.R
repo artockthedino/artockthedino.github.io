015e40e0b6dd8261e9ed128c8cfc6b8fba5615b6
artockthedino <chadedwardworley@gmail.com>
2026-03-23 22:28:27 -0400

    cubics

---
+++ algtwo2026/u09/0324/exams2forms_testing.R
@@ -0,0 +1,18 @@
+library("exams2forms")
+n = 10
+
+### Entrance ticket
+pm = list("x_cubic_from_zeros.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_0324",
+             dir = ".",
+             name="examples_0324")
+
+
+
+
+
+
+
