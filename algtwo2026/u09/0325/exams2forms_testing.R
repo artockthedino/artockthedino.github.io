fbe8875c36598149b4ddaedf73dde4f92e581eec
artockthedino <chadedwardworley@gmail.com>
2026-03-24 22:11:21 -0400

    cubics

---
+++ algtwo2026/u09/0325/exams2forms_testing.R
@@ -0,0 +1,18 @@
+library("exams2forms")
+n = 10
+
+### Entrance ticket
+pm = list("x_rational_factors_from_roots.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_0325",
+             dir = ".",
+             name="examples_0325")
+
+
+
+
+
+
+
---
+++ pbl/Lsys_turtle/lsys_gen/lsys_gen.R
@@ -0,0 +1,7 @@
+a = "AA"
+
+turn_frac = 1/4
+turn_angle = turn_frac*pi
+
+
+
