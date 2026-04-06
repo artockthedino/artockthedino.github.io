d394b157c86921e2d14abb3e8bb2986970529dd6
artockthedino <chadedwardworley@gmail.com>
2026-04-05 21:49:04 -0400

    cubics

--- algtwo2026/u10/0406/exams2forms_testing.R
+++ algtwo2026/u10/0406/exams2forms_testing.R
@@ -2,20 +2,13 @@ library("exams2forms")
 n = 12
 
 ### Entrance ticket
-pm = list("x_poly_sketch.Rmd",
-          "x_poly_sketch_dubs.Rmd",
-          "x_sketch_factored_poly.Rmd",
-          "x_match_graph_poly_eq.Rmd",
-          "x_quartic_standard_from_sketch.Rmd",
-          "x_min_deg_from_num_extrema.Rmd",
-          "x_max_num_extrema.Rmd",
-          "x_div_poly_5th_by_2nd.Rmd")
+pm = list("x_div_poly_5th_by_2nd.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0406",
+             title = "examples_0406_B",
              dir = ".",
-             name="examples_0406")
+             name="examples_0406_B")
 
 
 
