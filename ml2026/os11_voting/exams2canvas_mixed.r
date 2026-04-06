99ddee4d129db82c9b876eeed1ad1f30a750103c
artockthedino <chadedwardworley@gmail.com>
2026-04-06 09:48:20 -0400

    cubics

--- ml2026/os11_voting/exams2canvas_mixed.r
+++ ml2026/os11_voting/exams2canvas_mixed.r
@@ -3,29 +3,31 @@ n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-# pm = list("instant_runoff.Rmd",
+pm = list("condorcet_2.Rmd",
+    "borda_pnts_all_cans_2.Rmd",
+    "instant_runoff.Rmd",
+    "approval_2.Rmd")
+exams2canvas(pm,
+             n=n,
+             dir=outdir,
+             points=rep(3,length(pm)),
+             maxattempts = 5,
+             name="m0406_ET",
+             template = "canvas_qti12.xml",
+             cloze = list(enumerate = FALSE))
+
+# pm = list("condorcet.Rmd",
+#           "borda_pnts_all_cans.Rmd",
+#           "instant_runoff.Rmd",
 #           "approval.Rmd")
 # exams2canvas(pm,
 #              n=n,
 #              dir=outdir,
 #              points=rep(5,length(pm)),
-#              maxattempts = 5,
-#              name="m0403_ET",
+#              maxattempts = 6,
+#              name="m0403_voting_methods_review",
 #              template = "canvas_qti12.xml",
 #              cloze = list(enumerate = FALSE))
-
-pm = list("condorcet.Rmd",
-          "borda_pnts_all_cans.Rmd",
-          "instant_runoff.Rmd",
-          "approval.Rmd")
-exams2canvas(pm,
-             n=n,
-             dir=outdir,
-             points=rep(5,length(pm)),
-             maxattempts = 6,
-             name="m0403_voting_methods_review",
-             template = "canvas_qti12.xml",
-             cloze = list(enumerate = FALSE))
 
 
 # pm = list("standard_from_graph.Rmd",
--- ml2026/os11_voting/exams2forms_testing.R
+++ ml2026/os11_voting/exams2forms_testing.R
@@ -2,14 +2,14 @@ library("exams2forms")
 n = 10
 
 ### Entrance ticket
-pm = list("instant_runoff.Rmd",
-          "approval.Rmd",
-          "condorcet.Rmd",
-          "borda_pnts_all_cans.Rmd")
+pm = list("condorcet_2.Rmd",
+          "borda_pnts_all_cans_2.Rmd",
+          "instant_runoff.Rmd",
+          "approval_2.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0403",
+             title = "examples_0406",
              dir = ".",
-             name="examples_0403")
+             name="examples_0406")
 
