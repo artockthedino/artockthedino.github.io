22b98a44147cd58e4a095cf655259f0cbb448f7b
artockthedino <chadedwardworley@gmail.com>
2026-04-10 13:13:21 -0400

    sketch polynomials

--- pbl/API_tools/upload_QTI_zip.py
+++ pbl/API_tools/upload_QTI_zip.py
@@ -5,8 +5,8 @@ with open("//Users/chad.worley@bartcharter.org/Documents/canvasAPItests/acctok.t
   access_token = f.read()
 
 # Variables
-course_id = '1767'  ## ML=1945... PBL_Q4_ACE=1715... PBL_Q4_BD=1767
-file_path_list = ["outcanvas/pbl_day02_ET.zip"]
+course_id = '1715'  ## ML=1945... PBL_Q4_ACE=1715... PBL_Q4_BD=1767
+file_path_list = ["outcanvas/pbl_day3_ET.zip","outcanvas/pbl_day3_recognize_frieze.zip"]
 
 ######## FOR UPLOADING ETS
 
--- pbl/daily/d03/exams2forms_testing.R
+++ pbl/daily/d03/exams2forms_testing.R
@@ -2,11 +2,12 @@ library("exams2forms")
 n = 10
 
 ### Entrance ticket
-pm = list("x_recognize_Frieze_poly.Rmd")
+# pm = list("x_recognize_Frieze_poly.Rmd")
+pm = list("x_trarotglimir_noref.Rmd","x_trarotglimir_polygon.Rmd","x_trarotglimir_rainbownet.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_day03",
+             title = "examples_ET_day03",
              dir = ".",
-             name="examples_day03")
+             name="examples_ET_day03")
 
