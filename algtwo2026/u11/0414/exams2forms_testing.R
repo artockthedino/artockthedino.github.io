23f7133a74fa0d77c678a19f514e0855745b44f1
artockthedino <chadedwardworley@gmail.com>
2026-04-15 22:07:44 -0400

    base conversion

--- algtwo2026/u11/0414/exams2forms_testing.R
+++ algtwo2026/u11/0414/exams2forms_testing.R
@@ -1,18 +1,14 @@
 library("exams2forms")
-n = 5
+n = 500
 
 ### Entrance ticket
-pm = list("x_EZ_two-step_equations.Rmd",
-          "x_raise_grade_average.Rmd",
-          "23_round_trip_ave_speed.Rmd",
-          "24_lagrange_polynomial.Rmd",
-          "25_cubic_regression.Rmd")
+pm = list("x_EZ_two-step_equations.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0414",
+             title = "examples_EZ_two_step",
              dir = ".",
-             name="examples_0414")
+             name="examples_EZ_two_step")
 
 
 
