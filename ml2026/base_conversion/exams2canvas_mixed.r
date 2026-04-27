2f739c0f67ec4e192c5f65f4ed4318cb9b981d03
artockthedino <chadedwardworley@gmail.com>
2026-04-27 10:13:05 -0400

    inverse review

--- ml2026/base_conversion/exams2canvas_mixed.r
+++ ml2026/base_conversion/exams2canvas_mixed.r
@@ -3,29 +3,32 @@ n = 30
 outdir = "outcanvas"
 
 ### Entrance ticket
-pm = list("x_base_b_addition.Rmd",
-          "x_EZ_two-step_equations.Rmd")
-exams2canvas(pm,
-             n=n,
-             dir=outdir,
-             points=rep(3,length(pm)),
-             maxattempts = 5,
-             name="m0416_ET",
-             template = "canvas_qti12.xml",
-             cloze = list(enumerate = FALSE))
-
-pm = list("x_base_b_multiply.Rmd",
-          "x_base_b_multiply.Rmd",
-          "x_base_b_multiply.Rmd",
+pm = list("x_num_exp_form_to_normal.Rmd",
+          "x_alien_base_5.Rmd",
+          "x_base_10_to_alph.Rmd",
+          "x_base_b_addition.Rmd",
           "x_base_b_multiply.Rmd")
 exams2canvas(pm,
              n=n,
              dir=outdir,
-             points=rep(3,length(pm)),
+             points=rep(4,length(pm)),
              maxattempts = 5,
-             name="m0416_alien_multiplication",
+             name="m0427_ET",
              template = "canvas_qti12.xml",
              cloze = list(enumerate = FALSE))
+
+# pm = list("x_base_b_multiply.Rmd",
+#           "x_base_b_multiply.Rmd",
+#           "x_base_b_multiply.Rmd",
+#           "x_base_b_multiply.Rmd")
+# exams2canvas(pm,
+#              n=n,
+#              dir=outdir,
+#              points=rep(3,length(pm)),
+#              maxattempts = 5,
+#              name="m0416_alien_multiplication",
+#              template = "canvas_qti12.xml",
+#              cloze = list(enumerate = FALSE))
 
 
 # pm = list("standard_from_graph.Rmd",
--- ml2026/base_conversion/exams2forms_testing.R
+++ ml2026/base_conversion/exams2forms_testing.R
@@ -1,13 +1,16 @@
 library("exams2forms")
-n = 10
+n = 6
 
 ### Entrance ticket
-pm = list("x_base_b_addition.Rmd",
+pm = list("x_num_exp_form_to_normal.Rmd",
+          "x_alien_base_5.Rmd",
+          "x_base_10_to_alph.Rmd",
+          "x_base_b_addition.Rmd",
           "x_base_b_multiply.Rmd")
 
 exams2webquiz(pm,
              n=n,
-             title = "examples_0416",
+             title = "examples_0427",
              dir = ".",
-             name="examples_0416")
+             name="examples_0427")
 
