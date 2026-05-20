6c6257e0a216086d25e56ad76d421d98b115af64
Chad Worley <chad.worley@bartcharter.org@Chad-Worley-MacBook-Air-1561-2025.local>
2026-05-20 13:50:06 -0400

    money_practice

---
+++ pbl/Lsys_turtle/intro_ET/exams2forms_testing.R
@@ -0,0 +1,11 @@
+library("exams2forms")
+n = 10
+
+pm = list("replace_substring.Rmd","path_string.Rmd")
+
+exams2webquiz(pm,
+             n=n,
+             title = "examples_0318",
+             dir = ".",
+             name="examples_0318")
+
